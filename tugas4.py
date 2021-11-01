8431bed5d64411df49aebdd37d23a81154dee504
LemanArt <leman210101@gmail.com>
2021-11-01 22:50:33 +0700

    tambah file tugas4.py

---
+++ tugas4.py
@@ -0,0 +1,7 @@
+
+phi = 3.14
+r = float(input("Masukan Jari-jari : "))
+luas = phi*r**2
+keliling = 2*phi*r
+print ("Luas Lingkaran = ",phi,"*",r,"*",r,"=",luas)
+print ("Keliling = 2 *",phi,"*",r,"=",keliling)
